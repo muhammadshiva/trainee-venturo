5b35256ec1d09e5b61c66dc421c0f4fed031d9cd
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-16 08:51:37 +0700

    feat(constants) : add asset const

---
+++ lib/constants/core/asset_const.dart
@@ -0,0 +1,51 @@
+class AssetConst {
+  AssetConst._();
+
+  // Icon
+  static const String iconAddImage = 'assets/svg/ic_add_image.svg';
+  static const String iconApple = 'assets/svg/ic_apple.svg';
+  static const String iconCart = 'assets/svg/ic_cart.svg';
+  static const String iconCheck = 'assets/svg/ic_check.svg';
+  static const String iconDate = 'assets/svg/ic_date.svg';
+  static const String iconDiscount = 'assets/svg/ic_discount.svg';
+  static const String iconDrink = 'assets/svg/ic_drink.svg';
+  static const String iconEdit = 'assets/svg/ic_edit.svg';
+  static const String iconEmptyCart = 'assets/svg/ic_empty_cart.svg';
+  static const String iconFingerprint = 'assets/svg/ic_fingerprint.svg';
+  static const String iconFood = 'assets/svg/ic_food.svg';
+  static const String iconGoogle = 'assets/svg/ic_google.svg';
+  static const String iconHome = 'assets/svg/ic_home.svg';
+  static const String iconJavaCode = 'assets/images/ic_java_code.png';
+  static const String iconKtp = 'assets/svg/ic_ktp.svg';
+  static const String iconLevel = 'assets/svg/ic_level.svg';
+  static const String iconList = 'assets/svg/ic_list.svg';
+  static const String iconLoading = 'assets/svg/ic_loading.svg';
+  static const String iconLocation = 'assets/images/ic_location.png';
+  static const String iconNoData = 'assets/svg/ic_no_data.svg';
+  static const String iconNoInternet = 'assets/svg/ic_no_internet.svg';
+  static const String iconNote = 'assets/svg/ic_note.svg';
+  static const String iconOrder = 'assets/svg/ic_order.svg';
+  static const String iconOrderSuccess = 'assets/svg/ic_order_success.svg';
+  static const String iconPayment = 'assets/svg/ic_payment.svg';
+  static const String iconPrice = 'assets/svg/ic_price.svg';
+  static const String iconProfile = 'assets/svg/ic_profile.svg';
+  static const String iconPromo = 'assets/svg/ic_promo.svg';
+  static const String iconReply = 'assets/svg/ic_reply.svg';
+  static const String iconReview = 'assets/svg/ic_review.svg';
+  static const String iconSend = 'assets/svg/ic_send.svg';
+  static const String iconServerError = 'assets/svg/ic_server_error.svg';
+  static const String iconStar = 'assets/svg/ic_star.svg';
+  static const String iconStarEmpty = 'assets/svg/ic_star_empty.svg';
+  static const String iconTopping = 'assets/svg/ic_topping.svg';
+  static const String iconVoucher = 'assets/svg/ic_voucher.svg';
+
+  // Background
+  static const String bgPattern1 = 'assets/images/bg_pattern_1.png';
+  static const String bgPattern2 = 'assets/images/bg_pattern_2.png';
+  static const String bgProfile = 'assets/images/bg_profile.png';
+  static const String bgPromo = 'assets/images/bg_promo.png';
+
+  // Flag
+  static const String flagID = 'assets/svg/flag_id.svg';
+  static const String flagEN = 'assets/svg/flag_en.svg';
+}
