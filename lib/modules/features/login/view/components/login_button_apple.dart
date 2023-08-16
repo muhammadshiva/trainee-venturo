fd0c0edec97345f5cd44ccc4735c8de482709edc
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-16 14:22:00 +0700

    feat(features) : create login button by sosmed

---
+++ lib/modules/features/login/view/components/login_button_apple.dart
@@ -0,0 +1,51 @@
+import 'package:coffee_app/constants/core/asset_const.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:coffee_app/configs/theme/colors.dart';
+
+class LoginButtonApple extends StatelessWidget {
+  final void Function()? onPressed;
+
+  const LoginButtonApple({Key? key, this.onPressed}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        primary: AppColor.darkColor,
+        elevation: 4,
+        padding: EdgeInsets.symmetric(horizontal: 36.r, vertical: 14.r),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24.r),
+        ),
+      ),
+      child: Row(
+        children: [
+          SvgPicture.asset(AssetConst.iconApple, width: 24.r, height: 24.r),
+          const Spacer(),
+          Text(
+            'Login with'.tr,
+            style: GoogleFonts.montserrat(
+              fontSize: 14.sp,
+              fontWeight: FontWeight.w400,
+              height: 1.219,
+            ),
+          ),
+          Text(
+            ' Apple',
+            style: GoogleFonts.montserrat(
+              fontSize: 14.sp,
+              fontWeight: FontWeight.w600,
+              height: 1.219,
+            ),
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/modules/features/login/view/components/login_button_google.dart
@@ -0,0 +1,53 @@
+import 'package:coffee_app/configs/theme/colors.dart';
+import 'package:coffee_app/constants/core/asset_const.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginButtonGoogle extends StatelessWidget {
+  final void Function()? onPressed;
+
+  const LoginButtonGoogle({Key? key, this.onPressed}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        primary: Colors.white,
+        elevation: 4,
+        padding: EdgeInsets.symmetric(horizontal: 36.r, vertical: 14.r),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24.r),
+        ),
+      ),
+      child: Row(
+        children: [
+          SvgPicture.asset(AssetConst.iconGoogle, width: 24.r, height: 24.r),
+          const Spacer(),
+          Text(
+            'Login with'.tr,
+            style: GoogleFonts.montserrat(
+              fontSize: 14.sp,
+              fontWeight: FontWeight.w400,
+              color: AppColor.darkColor,
+              height: 1.219,
+            ),
+          ),
+          Text(
+            ' Google',
+            style: GoogleFonts.montserrat(
+              fontSize: 14.sp,
+              fontWeight: FontWeight.w600,
+              color: AppColor.darkColor,
+              height: 1.219,
+            ),
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
+  }
+}
