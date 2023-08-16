78153a624ae4a7c9a897f30d4d9cac37de65a324
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-16 09:57:12 +0700

    feat(features) : add splash controller

---
+++ lib/modules/features/splash/controllers/splash_controller.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+
+class SplashController extends GetxController {
+  static SplashController get to => Get.find<SplashController>();
+
+  @override
+  void onInit() async {
+    super.onInit();
+
+    // Delay 1 second
+    await Future.delayed(const Duration(seconds: 1));
+  }
+}
