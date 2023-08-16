4aa7c1dc131279fd5cf81c0338e85994d87e4c0b
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-16 14:22:19 +0700

    feat(features) : create divider text component

---
+++ lib/modules/features/login/view/components/divider_with_text.dart
@@ -0,0 +1,28 @@
+import 'package:coffee_app/configs/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+class DividerWithText extends StatelessWidget {
+  final String text;
+
+  const DividerWithText(this.text, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(child: Divider(color: Colors.black.withOpacity(0.25))),
+        15.horizontalSpaceRadius,
+        Text(
+          text.tr,
+          style: Get.textTheme.bodySmall!.copyWith(
+            color: AppColor.darkColor.withOpacity(0.25),
+          ),
+        ),
+        15.horizontalSpaceRadius,
+        Expanded(child: Divider(color: Colors.black.withOpacity(0.25))),
+      ],
+    );
+  }
+}
