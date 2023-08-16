67e8260009d01a2c5e13279a9603c94ae7b07d6f
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-16 09:54:12 +0700

    refactor : refactoring main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+import 'package:coffee_app/configs/pages/app_pages.dart';
+import 'package:coffee_app/configs/routes/app_routes.dart';
+import 'package:coffee_app/configs/theme/light_theme.dart';
+import 'package:coffee_app/constants/commons/constants.dart';
+import 'package:coffee_app/modules/global_controller/global_binding.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,59 +16,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    // Screen Util Init based on application design size
+    return ScreenUtilInit(
+      designSize: AppConst.appDesignSize,
+      builder: (context, child) => GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialBinding: GlobalBinding(),
+        title: AppConst.appName,
+        initialRoute: AppRoutes.splashView,
+        getPages: AppPages.pages(),
+        theme: AppTheme.lightTheme,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
