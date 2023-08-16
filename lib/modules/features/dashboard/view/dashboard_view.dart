2d5be781f13ee0d64a0d3f1a63ef3455486bb5a7
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-16 15:30:43 +0700

    feat(features) : add logout button

--- lib/modules/features/dashboard/view/dashboard_view.dart
+++ lib/modules/features/dashboard/view/dashboard_view.dart
@@ -1,14 +1,33 @@
+import 'package:coffee_app/modules/features/login/controllers/login_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class DashboardView extends StatelessWidget {
-  const DashboardView({super.key});
+  DashboardView({super.key});
+
+  final loginController = Get.put(LoginController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Text('Dashboard Page'),
+        child: Center(
+          child: Text('Dashboard Page'),
+        ),
       ),
+      floatingActionButton: Container(
+        margin: const EdgeInsets.only(bottom: 20),
+        width: 300,
+        child: ElevatedButton(
+          onPressed: () {
+            loginController.logout();
+          },
+          child: Text(
+            'Logout',
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
