220b305f5d1ff1a9d2d30573c8a6861e16b5b16a
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-16 13:57:00 +0700

    feat(widget) : add primary button widgets

---
+++ lib/shared/widgets/primary_button.dart
@@ -0,0 +1,56 @@
+import 'package:coffee_app/configs/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class PrimaryButton extends StatelessWidget {
+  final String text;
+  final void Function()? onPressed;
+  final VisualDensity? visualDensity;
+  final bool isCompact;
+
+  const PrimaryButton({
+    Key? key,
+    required this.text,
+    this.onPressed,
+    this.visualDensity,
+  })  : isCompact = false,
+        super(key: key);
+
+  const PrimaryButton.compact({
+    Key? key,
+    required this.text,
+    this.onPressed,
+  })  : visualDensity = VisualDensity.compact,
+        isCompact = true,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        visualDensity: visualDensity,
+        primary: AppColor.blueColor,
+        elevation: 3,
+        textStyle: GoogleFonts.montserrat(
+          fontSize: isCompact ? 10.sp : 14.sp,
+          fontWeight: FontWeight.w800,
+          color: Colors.white,
+          height: 1.219,
+        ),
+        tapTargetSize: isCompact ? MaterialTapTargetSize.shrinkWrap : null,
+        minimumSize: isCompact ? Size(100.r, 40.r) : null,
+        padding: EdgeInsets.symmetric(
+          horizontal: isCompact ? 18.r : 24.r,
+          vertical: isCompact ? 0.r : 14.r,
+        ),
+        shape: RoundedRectangleBorder(
+          side: const BorderSide(color: AppColor.darkBlueColor, width: 1),
+          borderRadius: BorderRadius.circular(24),
+        ),
+      ),
+      child: Text(text),
+    );
+  }
+}
