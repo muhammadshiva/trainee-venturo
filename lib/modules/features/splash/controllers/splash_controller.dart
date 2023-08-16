3578d53cd5233628efe32662ae32cbbe42c00344
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-16 15:30:19 +0700

    feat(features) : add login session

--- lib/modules/features/splash/controllers/splash_controller.dart
+++ lib/modules/features/splash/controllers/splash_controller.dart
@@ -1,3 +1,4 @@
+import 'package:coffee_app/utils/services/local_db_services.dart';
 import 'package:get/get.dart';
 
 class SplashController extends GetxController {
@@ -10,7 +11,17 @@ class SplashController extends GetxController {
     // Delay 1 second
     await Future.delayed(const Duration(seconds: 1));
 
-    // Access Login Page
-    Get.toNamed('/login');
+    // Mendapatkan user dan token dari local DB service
+    var user = await LocalDBServices.getUser();
+    var token = await LocalDBServices.getToken();
+
+    // If there is login session
+    if (user != null && token != null) {
+      // Going to main page
+      await Get.offAllNamed('/dashboard');
+    } else {
+      // if no session go to login page
+      Get.offAllNamed('/login');
+    }
   }
 }
