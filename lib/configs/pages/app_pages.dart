18152f0236426e681bd6bf04da5aab6910c37a19
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-16 14:17:42 +0700

    feat(configs) : add login and dashboard

--- lib/configs/pages/app_pages.dart
+++ lib/configs/pages/app_pages.dart
@@ -1,4 +1,8 @@
 import 'package:coffee_app/configs/routes/app_routes.dart';
+import 'package:coffee_app/modules/features/dashboard/controllers/dashboard_binding.dart';
+import 'package:coffee_app/modules/features/dashboard/view/dashboard_view.dart';
+import 'package:coffee_app/modules/features/login/controllers/login_binding.dart';
+import 'package:coffee_app/modules/features/login/view/ui/login_view.dart';
 import 'package:coffee_app/modules/features/splash/controllers/splash_binding.dart';
 import 'package:coffee_app/modules/features/splash/view/ui/splash_view.dart';
 import 'package:get/get.dart';
@@ -15,6 +19,19 @@ class AppPages {
         page: () => SplashView(),
         binding: SplashBinding(),
       ),
+
+      GetPage(
+        name: AppRoutes.loginView,
+        page: () => LoginView(),
+        binding: LoginBinding(),
+      ),
+
+      // Dashboard
+      GetPage(
+        name: AppRoutes.loginView,
+        page: () => DashboardView(),
+        binding: DashboardBinding(),
+      )
     ];
   }
 }
