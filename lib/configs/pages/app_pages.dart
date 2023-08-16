831210c53adb66f0219af4e8a945e66956d97feb
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-16 08:52:47 +0700

    feat(configs) : add pages

---
+++ lib/configs/pages/app_pages.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+class AppPages {
+  // Ensures that this class cannot be instantiated
+  AppPages._();
+
+  static List<GetPage> pages() {
+    return [
+      // Authentication
+    ];
+  }
+}
