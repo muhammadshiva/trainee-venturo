a4f8d3a866d09b4ead89047a63f3f480cfe675fe
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-16 14:20:45 +0700

    feat(features) : create login binding

---
+++ lib/modules/features/login/controllers/login_binding.dart
@@ -0,0 +1,9 @@
+import 'package:coffee_app/modules/features/login/controllers/login_controller.dart';
+import 'package:get/get.dart';
+
+class LoginBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.put(LoginController());
+  }
+}
