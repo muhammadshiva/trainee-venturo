fd4e064108879c302396f5e6ea0ddc32bc3814ef
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-16 14:23:45 +0700

    feat(features) : create textfield email & password

---
+++ lib/modules/features/login/view/components/text_field_email.dart
@@ -0,0 +1,43 @@
+import 'package:coffee_app/configs/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class TextFieldEmail extends StatelessWidget {
+  final TextEditingController controller;
+
+  const TextFieldEmail({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      style: Get.textTheme.bodySmall,
+      decoration: InputDecoration(
+        labelText: 'Email'.tr,
+        labelStyle: Get.textTheme.bodySmall,
+        hintText: 'Lorem.ipsum@gmail.com',
+        hintStyle: Get.textTheme.bodySmall!.copyWith(
+          color: AppColor.darkColor.withOpacity(0.25),
+        ),
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        enabledBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: AppColor.blueColor, width: 1),
+        ),
+        focusedBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: AppColor.blueColor, width: 1),
+        ),
+      ),
+      keyboardType: TextInputType.emailAddress,
+      textInputAction: TextInputAction.next,
+      validator: (value) {
+        if (!GetUtils.isEmail(value ?? '')) {
+          return 'Email is not valid'.tr;
+        }
+        return null;
+      },
+    );
+  }
+}
---
+++ lib/modules/features/login/view/components/text_field_password.dart
@@ -0,0 +1,45 @@
+import 'package:coffee_app/configs/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class TextFieldPassword extends StatelessWidget {
+  final TextEditingController controller;
+
+  const TextFieldPassword({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      style: Get.textTheme.bodySmall,
+      decoration: InputDecoration(
+        labelText: 'Password'.tr,
+        labelStyle: Get.textTheme.bodySmall,
+        hintText: '****************',
+        hintStyle: Get.textTheme.bodySmall!.copyWith(
+          color: AppColor.darkColor.withOpacity(0.25),
+        ),
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        enabledBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: AppColor.blueColor, width: 1),
+        ),
+        focusedBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: AppColor.blueColor, width: 1),
+        ),
+      ),
+      obscureText: true,
+      keyboardType: TextInputType.visiblePassword,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Password is required'.tr;
+        } else if (value.length < 5) {
+          return 'Password must be at least 5 characters'.tr;
+        }
+        return null;
+      },
+    );
+  }
+}
