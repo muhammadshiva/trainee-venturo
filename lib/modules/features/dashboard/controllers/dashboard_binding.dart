7d4e48002d4342569c5f9e35734bb61a1e1ab608
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-17 11:35:19 +0700

    feat(features) : bindings home, order & profile

--- lib/modules/features/dashboard/controllers/dashboard_binding.dart
+++ lib/modules/features/dashboard/controllers/dashboard_binding.dart
@@ -1,4 +1,7 @@
 import 'package:coffee_app/modules/features/dashboard/controllers/dashboard_controller.dart';
+import 'package:coffee_app/modules/features/home/controllers/home_controller.dart';
+import 'package:coffee_app/modules/features/order/controllers/order_controller.dart';
+import 'package:coffee_app/modules/features/profile/controllers/profile_controller.dart';
 import 'package:get/get.dart';
 
 class DashboardBinding extends Bindings {
@@ -6,5 +9,12 @@ class DashboardBinding extends Bindings {
   void dependencies() {
     // Initialize dashboard controller
     Get.put(DashboardController());
+
+    // Initialize cart controller
+
+    /// Inisialisasi home, order, profile
+    Get.lazyPut(() => HomeController());
+    Get.lazyPut(() => OrderController());
+    Get.lazyPut(() => ProfileController());
   }
 }
