7c50bc0ed6bb2a824f4ffb13343cd0d55972273b
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-18 08:23:40 +0700

    feat(constants) : add api consts

--- lib/constants/core/api_const.dart
+++ lib/constants/core/api_const.dart
@@ -10,5 +10,29 @@ class ApiConst {
 
   // Home
   static const String allPromo = '$baseUrl/promo/all';
+  static const String allPromoPerUser = '$baseUrl/promo/user';
+  static const String detailPromo = '$baseUrl/promo/detail';
   static const String allMenu = '$baseUrl/menu/all';
+  static const String detailMenu = '$baseUrl/menu/detail';
+
+  /// Cart
+  static const String allDiscountPerUser = '$baseUrl/diskon/user';
+  static const String allVoucherPerUser = '$baseUrl/voucher/user';
+
+  /// Order
+  static const String addOrder = '$baseUrl/order/add';
+  static const String onGoingOrder = '$baseUrl/order/user';
+  static const String historyOrder = '$baseUrl/order/history';
+  static const String detailOrder = '$baseUrl/order/detail';
+  static const String cancelOrder = '$baseUrl/order/batal';
+
+  /// Profile
+  static const String detailUser = '$baseUrl/user/detail';
+  static const String updateUser = '$baseUrl/user/update';
+  static const String updateUserPhoto = '$baseUrl/user/profil';
+  static const String updateUserKTP = '$baseUrl/user/ktp';
+
+  /// Review
+  static const String allReview = '$baseUrl/review';
+  static const String addReview = '$baseUrl/review/add';
 }
