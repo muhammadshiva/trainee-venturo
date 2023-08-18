228c9b3c0070afde6b38de2cd3d22ba812f12908
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-18 08:26:45 +0700

    feat(features) : initialize uni links

--- lib/modules/global_controller/global_controller.dart
+++ lib/modules/global_controller/global_controller.dart
@@ -1,8 +1,10 @@
 import 'package:coffee_app/configs/routes/app_routes.dart';
 import 'package:coffee_app/shared/widgets/network_error_view.dart';
+import 'package:coffee_app/utils/services/local_db_services.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:uni_links/uni_links.dart';
 
 class GlobalController extends GetxController {
   static GlobalController get to => Get.find();
@@ -27,6 +29,9 @@ class GlobalController extends GetxController {
   /// To check internet connection
   void checkConnectivity() {
     _connectivity.checkConnectivity().then(_updateConnectivity);
+
+    /// Inisialisasi uni links
+    initUniLinks();
   }
 
   /// To update internet connection
@@ -58,5 +63,26 @@ class GlobalController extends GetxController {
       content: const NetworkErrorView(),
       barrierDismissible: false,
     );
+  }
+
+  Future<void> initUniLinks() async {
+    /// Listen dari perubahan uni link
+    uriLinkStream.listen(processUniLinks);
+  }
+
+  void processUniLinks(Uri? uri) async {
+    if (uri != null && uri.queryParameters['id_promo'] != null) {
+      /// Mendapatkan user dan token dari local DB service
+      var user = await LocalDBServices.getUser();
+      var token = await LocalDBServices.getToken();
+
+      if (user != null && token != null) {
+        /// Navigasi ke detail promo jika sudah login
+        await Get.toNamed(
+          AppRoutes.detailPromoView,
+          arguments: int.parse(uri.queryParameters['id_promo']!),
+        );
+      }
+    }
   }
 }
