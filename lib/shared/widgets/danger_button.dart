e3118099febf69b86bbb6269033de38401c2825d
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-18 08:18:17 +0700

    feat(shared) : danger button

---
+++ lib/shared/widgets/danger_button.dart
@@ -0,0 +1,38 @@
+import 'package:coffee_app/configs/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class DangerButton extends StatelessWidget {
+  final String text;
+  final void Function()? onPressed;
+
+  const DangerButton({
+    Key? key,
+    required this.text,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        primary: AppColor.redColor,
+        elevation: 3,
+        textStyle: GoogleFonts.montserrat(
+          fontSize: 14.sp,
+          fontWeight: FontWeight.w800,
+          color: Colors.white,
+          height: 1.219,
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 24.r, vertical: 14.r),
+        shape: RoundedRectangleBorder(
+          side: const BorderSide(color: AppColor.darkRedColor, width: 1),
+          borderRadius: BorderRadius.circular(24),
+        ),
+      ),
+      child: Text(text),
+    );
+  }
+}
