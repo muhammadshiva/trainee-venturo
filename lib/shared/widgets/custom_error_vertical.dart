cd879d1df74e2e755bbacd824b24e259370a4aca
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-18 08:18:31 +0700

    feat(shared) : custom error vertical

---
+++ lib/shared/widgets/custom_error_vertical.dart
@@ -0,0 +1,40 @@
+import 'package:coffee_app/constants/core/asset_const.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+
+class CustomErrorVertical extends StatelessWidget {
+  final String message;
+
+  const CustomErrorVertical({
+    Key? key,
+    required this.message,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          /// Icon server error
+          SvgPicture.asset(
+            AssetConst.iconServerError,
+            width: 250.r,
+          ),
+          15.verticalSpacingRadius,
+
+          /// Pesan error
+          Text(
+            message,
+            style: Get.textTheme.bodyMedium,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
