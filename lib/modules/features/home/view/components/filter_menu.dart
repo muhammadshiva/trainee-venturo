6bb951bc7cd02338f89c78cd970c3da92e17df1d
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-18 08:24:28 +0700

    feat(features) : add filter menu

---
+++ lib/modules/features/home/view/components/filter_menu.dart
@@ -0,0 +1,58 @@
+import 'package:coffee_app/configs/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+
+class FilterMenu extends StatelessWidget {
+  final bool isSelected;
+  final String text;
+  final String iconPath;
+  final Function()? onTap;
+
+  const FilterMenu({
+    Key? key,
+    this.isSelected = false,
+    required this.text,
+    required this.iconPath,
+    this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      borderRadius: BorderRadius.circular(30.r),
+      child: Ink(
+        padding: EdgeInsets.symmetric(horizontal: 14.r),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30.r),
+          color: isSelected ? AppColor.darkColor2 : AppColor.blueColor,
+          boxShadow: [
+            BoxShadow(
+              offset: const Offset(0, 2),
+              blurRadius: 8,
+              spreadRadius: -1,
+              color: AppColor.darkColor2.withOpacity(0.25),
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            SvgPicture.asset(iconPath),
+            10.horizontalSpaceRadius,
+            Padding(
+              padding: EdgeInsets.only(bottom: 2.r),
+              child: Text(
+                text,
+                style: Get.textTheme.headlineSmall!.copyWith(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
