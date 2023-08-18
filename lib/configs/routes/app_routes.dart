083cee14520eac60df5acab2a0132cf5dd37172a
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-18 08:22:49 +0700

    feat(configs) : add routes

--- lib/configs/routes/app_routes.dart
+++ lib/configs/routes/app_routes.dart
@@ -8,4 +8,21 @@ class AppRoutes {
 
   // Home
   static const String dashboardView = '/dashboard';
+
+  /// Menu dan Promo
+  static const String detailPromoView = '/dashboard/detail-promo';
+  static const String detailMenuView = '/dashboard/detail-menu';
+
+  /// Cart
+  static const String cartView = '/cart';
+  static const String chooseVoucherView = '/cart/choose-voucher';
+  static const String detailVoucherView = '/cart/choose-voucher/detail-voucher';
+
+  /// Order
+  static const String detailOrderView = '/order/detail-order';
+
+  /// Review
+  static const String reviewView = '/review/';
+  static const String addReviewView = '/review/add-review';
+  static const String replyReviewView = '/review/reply-review';
 }
