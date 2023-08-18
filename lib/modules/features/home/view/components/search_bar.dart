1e81e98f4e508543e8b2075f7fec9f01a86ec8d7
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-18 08:25:17 +0700

    feat(features) : add search bar

---
+++ lib/modules/features/home/view/components/search_bar.dart
@@ -0,0 +1,46 @@
+import 'package:coffee_app/configs/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+class SearchBar extends StatelessWidget {
+  final TextEditingController controller;
+  final Function(String)? onChanged;
+
+  const SearchBar({
+    Key? key,
+    required this.controller,
+    required this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 8.r, horizontal: 10.r),
+      decoration: BoxDecoration(
+        border: Border.all(color: AppColor.blueColor),
+        borderRadius: BorderRadius.circular(30.r),
+      ),
+      child: Row(
+        children: [
+          Icon(Icons.search, color: AppColor.blueColor, size: 30.r),
+          10.horizontalSpaceRadius,
+          Expanded(
+            child: TextField(
+              controller: controller,
+              onChanged: onChanged,
+              style: Get.textTheme.headlineSmall,
+              decoration: InputDecoration.collapsed(
+                hintText: 'Search'.tr,
+                hintStyle: Get.textTheme.headlineSmall!.copyWith(
+                  color: const Color(0xFFAAAAAA),
+                ),
+              ),
+              textInputAction: TextInputAction.search,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
