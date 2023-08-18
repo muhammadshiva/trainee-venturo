cb70f561562bef5725e94be92fc4d71652d08099
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-18 08:22:35 +0700

    feat(configs) : add pages

--- lib/configs/pages/app_pages.dart
+++ lib/configs/pages/app_pages.dart
@@ -3,6 +3,10 @@ import 'package:coffee_app/modules/features/dashboard/controllers/dashboard_bind
 import 'package:coffee_app/modules/features/dashboard/view/dashboard_view.dart';
 import 'package:coffee_app/modules/features/login/controllers/login_binding.dart';
 import 'package:coffee_app/modules/features/login/view/ui/login_view.dart';
+import 'package:coffee_app/modules/features/menu/controllers/detail_menu_binding.dart';
+import 'package:coffee_app/modules/features/menu/view/ui/detail_menu_view.dart';
+import 'package:coffee_app/modules/features/promo/controllers/detail_promo_binding.dart';
+import 'package:coffee_app/modules/features/promo/ui/detail_promo_view.dart';
 import 'package:coffee_app/modules/features/splash/controllers/splash_binding.dart';
 import 'package:coffee_app/modules/features/splash/view/ui/splash_view.dart';
 import 'package:get/get.dart';
@@ -31,7 +35,19 @@ class AppPages {
         name: AppRoutes.dashboardView,
         page: () => DashboardView(),
         binding: DashboardBinding(),
-      )
+      ),
+
+      /// Menu dan Promo
+      GetPage(
+        name: AppRoutes.detailPromoView,
+        page: () => DetailPromoView(),
+        binding: DetailPromoBinding(),
+      ),
+      GetPage(
+        name: AppRoutes.detailMenuView,
+        page: () => DetailMenuView(),
+        binding: DetailMenuBinding(),
+      ),
     ];
   }
 }
