3256a89c4cbe461b3b83b9051e40019fe6a052be
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-20 21:42:28 +0700

    update location to venturo

--- lib/constants/commons/constants.dart
+++ lib/constants/commons/constants.dart
@@ -14,8 +14,8 @@ class AppConst {
       'exCGEA6WQg2IY0A55lwWM2:APA91bGZgAze0QPHNueGZS1Rot94v2dtYLxl_SV9GGOGs4ChA840SRTFUM36idgT5LNm8ud2WQuUooD_VzkWwAHP831wOg4YsiPF1zWajjPqwpvfHfjU2PwqV--FrpXZ4wjdexck8Roe';
 
   // Location information
-  static const double locationLatitude = -7.877427;
-  static const double locationLongitude = 112.6698827;
+  static const double locationLatitude = -7.9423553;
+  static const double locationLongitude = 112.620389;
   static const double maximumDistance = 100;
 
 // Menu information
