a124d76816538b60d5e38f8ed5b1aa2bc4152fb5
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-20 21:31:57 +0700

    feat(shared) : add widgets

---
+++ lib/modules/models/detail_order.dart
@@ -0,0 +1,52 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:coffee_app/constants/commons/constants.dart';
+import 'package:equatable/equatable.dart';
+
+class DetailOrder extends Equatable {
+  final int id_menu;
+  final String kategori;
+  final String topping;
+  final String nama;
+  final String? foto;
+  final int jumlah;
+  final String harga;
+  final int total;
+  final String catatan;
+
+  const DetailOrder({
+    required this.id_menu,
+    required this.kategori,
+    required this.topping,
+    required this.nama,
+    required this.foto,
+    required this.jumlah,
+    required this.harga,
+    required this.total,
+    required this.catatan,
+  });
+
+  /// Apakah menu ini adalah makanan
+  bool get isFood => kategori == AppConst.foodCategory;
+
+  /// Apakah menu ini adalah minuman
+  bool get isDrink => kategori == AppConst.drinkCategory;
+
+  /// From json
+  factory DetailOrder.fromJson(Map<String, dynamic> json) {
+    return DetailOrder(
+      id_menu: json['id_menu'] as int,
+      kategori: json['kategori'] as String,
+      topping: json['topping'] as String,
+      nama: json['nama'] as String,
+      foto: json['foto'] as String?,
+      jumlah: json['jumlah'] as int,
+      harga: json['harga'] as String,
+      total: json['total'] as int,
+      catatan: json['catatan'] as String,
+    );
+  }
+
+  @override
+  List<Object?> get props => [];
+}
---
+++ lib/shared/widgets/server_error_list_view.dart
@@ -0,0 +1,41 @@
+import 'package:coffee_app/constants/core/asset_const.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+
+class ServerErrorListView extends StatelessWidget {
+  const ServerErrorListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        /// Listview
+        ListView(),
+        Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              /// Icon server error
+              SvgPicture.asset(
+                AssetConst.iconServerError,
+                width: 250.r,
+              ),
+              25.verticalSpacingRadius,
+
+              /// Teks server error
+              Text(
+                'Server error'.tr,
+                style: Get.textTheme.bodyMedium,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
