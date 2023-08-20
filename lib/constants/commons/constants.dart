485643a40431b29468556bed0f94014660d76403
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-20 21:22:36 +0700

    feat(constants) : add app informations

--- lib/constants/commons/constants.dart
+++ lib/constants/commons/constants.dart
@@ -9,6 +9,9 @@ class AppConst {
   static const Size appDesignSize = Size(428, 926);
   static const String appLink = 'https://promo.trainee.landa.id';
   static const String appDeepLink = 'landa://javacode.app';
+  static const String appVersion = '1.0.0';
+  static const String firebaseCloudMessagingKey =
+      'exCGEA6WQg2IY0A55lwWM2:APA91bGZgAze0QPHNueGZS1Rot94v2dtYLxl_SV9GGOGs4ChA840SRTFUM36idgT5LNm8ud2WQuUooD_VzkWwAHP831wOg4YsiPF1zWajjPqwpvfHfjU2PwqV--FrpXZ4wjdexck8Roe';
 
   // Location information
   static const double locationLatitude = -7.877427;
@@ -20,4 +23,8 @@ class AppConst {
       'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/240px-No_image_available.svg.png';
   static const String foodCategory = 'makanan';
   static const String drinkCategory = 'minuman';
+
+  /// Informasi kondisi
+  static const String fingerprint = 'fingerprint';
+  static const String pin = 'pin';
 }
