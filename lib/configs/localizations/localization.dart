d69f7b1cfb87ea94a12048cb39005e006e455f6b
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-20 21:21:17 +0700

    feat(config) : add localizations

--- lib/configs/localizations/localization.dart
+++ lib/configs/localizations/localization.dart
@@ -1,3 +1,6 @@
+import 'package:coffee_app/configs/localizations/langs/en_us.dart';
+import 'package:coffee_app/configs/localizations/langs/id_id.dart';
+import 'package:coffee_app/constants/core/asset_const.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -9,12 +12,52 @@ class Localization extends Translations {
   /// Mengatur fallback locale
   static const fallbackLocale = Locale('en', 'US');
 
+  /// Daftar bahasa yang didukung
+  static const langs = [
+    'English',
+    'Indonesia',
+  ];
+
+  /// Daftar path asset dari bahasa yang didukung
+  static const flags = [
+    AssetConst.flagEN,
+    AssetConst.flagID,
+  ];
+
+  /// Daftar locale dari bahasa yang didukung
+  static const locales = [
+    Locale('en', 'US'),
+    Locale('id', 'ID'),
+  ];
+
+  /// Peta lokalisasi bahasa
+  @override
+  Map<String, Map<String, String>> get keys => {
+        'en_US': translations_en_US,
+        'id_ID': translations_id_ID,
+      };
+
+  /// Mengubah lokale
+  static void changeLocale(String lang) {
+    final locale = getLocaleFromLanguage(lang);
+    Get.updateLocale(locale);
+  }
+
+  /// Mencari bahasa dan mengembalikan locale
+  static Locale getLocaleFromLanguage(String lang) {
+    for (int i = 0; i < langs.length; i++) {
+      if (lang == langs[i]) return locales[i];
+    }
+    return currentLocale;
+  }
+
   /// Mengembalikan locale saat ini
   static Locale get currentLocale {
     return Get.locale ?? fallbackLocale;
   }
 
-  @override
-  // TODO: implement keys
-  Map<String, Map<String, String>> get keys => throw UnimplementedError();
+  /// Mengembalikan bahasa saat ini
+  static String get currentLanguage {
+    return langs.elementAt(locales.indexOf(currentLocale));
+  }
 }
