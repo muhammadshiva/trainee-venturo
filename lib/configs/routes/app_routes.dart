8ecc2eedbf5027dc015fdae7efbc04a6162af86d
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-20 21:21:42 +0700

    feat(config) : add routes

--- lib/configs/routes/app_routes.dart
+++ lib/configs/routes/app_routes.dart
@@ -25,4 +25,7 @@ class AppRoutes {
   static const String reviewView = '/review/';
   static const String addReviewView = '/review/add-review';
   static const String replyReviewView = '/review/reply-review';
+
+  /// Privacy Policy Web View
+  static const String privacyPolicy = '/privacy-policy';
 }
