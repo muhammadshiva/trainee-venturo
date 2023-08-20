057f1224a3ffe4854555557b19301ed4c05ff362
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-20 21:21:27 +0700

    feat(config) : add pages

--- lib/configs/pages/app_pages.dart
+++ lib/configs/pages/app_pages.dart
@@ -1,10 +1,16 @@
 import 'package:coffee_app/configs/routes/app_routes.dart';
+import 'package:coffee_app/modules/features/cart/view/ui/cart_view.dart';
+import 'package:coffee_app/modules/features/cart/view/ui/choose_voucher_view.dart';
+import 'package:coffee_app/modules/features/cart/view/ui/detail_voucher_view.dart';
 import 'package:coffee_app/modules/features/dashboard/controllers/dashboard_binding.dart';
 import 'package:coffee_app/modules/features/dashboard/view/dashboard_view.dart';
 import 'package:coffee_app/modules/features/login/controllers/login_binding.dart';
 import 'package:coffee_app/modules/features/login/view/ui/login_view.dart';
 import 'package:coffee_app/modules/features/menu/controllers/detail_menu_binding.dart';
 import 'package:coffee_app/modules/features/menu/view/ui/detail_menu_view.dart';
+import 'package:coffee_app/modules/features/order/controllers/detail_order_binding.dart';
+import 'package:coffee_app/modules/features/order/view/ui/detail_order_view.dart';
+import 'package:coffee_app/modules/features/profile/view/ui/privacy_policy_view.dart';
 import 'package:coffee_app/modules/features/promo/controllers/detail_promo_binding.dart';
 import 'package:coffee_app/modules/features/promo/ui/detail_promo_view.dart';
 import 'package:coffee_app/modules/features/splash/controllers/splash_binding.dart';
@@ -48,6 +54,32 @@ class AppPages {
         page: () => DetailMenuView(),
         binding: DetailMenuBinding(),
       ),
+
+      GetPage(
+        name: AppRoutes.detailOrderView,
+        page: () => DetailOrderView(),
+        binding: DetailOrderBinding(),
+      ),
+
+      /// Cart
+      GetPage(
+        name: AppRoutes.cartView,
+        page: () => CartView(),
+      ),
+      GetPage(
+        name: AppRoutes.chooseVoucherView,
+        page: () => ChooseVoucherView(),
+      ),
+      GetPage(
+        name: AppRoutes.detailVoucherView,
+        page: () => DetailVoucherView(),
+      ),
+
+      /// Privacy & Policy
+      GetPage(
+        name: AppRoutes.privacyPolicy,
+        page: () => const PrivacyPolicyView(),
+      )
     ];
   }
 }
