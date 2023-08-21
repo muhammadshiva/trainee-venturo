f8b8b3a83087536c5939669d23398f4f69482ef9
muhammadshiva <shivamuhammad07@gmail.com>
2023-08-21 14:50:32 +0700

    add condition to pop up dialog locations

--- lib/modules/features/dashboard/view/get_location_view.dart
+++ lib/modules/features/dashboard/view/get_location_view.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_conditional_rendering/flutter_conditional_rendering.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class GetLocationView extends StatefulWidget {
   const GetLocationView({Key? key}) : super(key: key);
@@ -21,95 +22,165 @@ class _GetLocationViewState extends State<GetLocationView> {
     return Scaffold(
       body: WillPopScope(
         onWillPop: () async => false,
-        child: Container(
+        child: Obx(
+          () => DashboardController.to.messageLocation.value == "Location service not enabled" ||
+                  DashboardController.to.messageLocation.value == "Layanan lokasi dimatikan."
+              ? activateLocation()
+              : getLocation(),
+        ),
+      ),
+    );
+  }
+
+  Widget activateLocation() {
+    return Container(
+      alignment: Alignment.center,
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(AssetConst.bgPattern2),
+          fit: BoxFit.fitHeight,
           alignment: Alignment.center,
-          width: double.infinity,
-          height: double.infinity,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(AssetConst.bgPattern2),
-              fit: BoxFit.fitHeight,
-              alignment: Alignment.center,
+        ),
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 30.r),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(AssetConst.activateLocation),
+          const SizedBox(height: 10),
+          Text(
+            'Aktifkan lokasimu',
+            style: GoogleFonts.montserrat().copyWith(
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () => AppSettings.openAppSettings(type: AppSettingsType.location),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.white,
+              elevation: 2,
+              padding: EdgeInsets.zero,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(24.r),
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.settings,
+                  color: AppColor.darkColor,
+                ),
+                16.horizontalSpaceRadius,
+                Text(
+                  'Open settings'.tr,
+                  style: Get.textTheme.bodySmall,
+                ),
+              ],
             ),
           ),
-          padding: EdgeInsets.symmetric(horizontal: 30.r),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
+        ],
+      ),
+    );
+  }
+
+  Widget getLocation() {
+    return Container(
+      alignment: Alignment.center,
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(AssetConst.bgPattern2),
+          fit: BoxFit.fitHeight,
+          alignment: Alignment.center,
+        ),
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 30.r),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Searching location...'.tr,
+            style: Get.textTheme.titleLarge!.copyWith(
+              color: AppColor.darkColor.withOpacity(0.5),
+            ),
+            textAlign: TextAlign.center,
+          ),
+          50.verticalSpacingRadius,
+          Stack(
             children: [
-              Text(
-                'Searching location...'.tr,
-                style: Get.textTheme.titleLarge!.copyWith(
-                  color: AppColor.darkColor.withOpacity(0.5),
-                ),
-                textAlign: TextAlign.center,
-              ),
-              50.verticalSpacingRadius,
-              Stack(
-                children: [
-                  Image.asset(AssetConst.iconLocation, width: 190.r),
-                  Padding(
-                    padding: EdgeInsets.all(70.r),
-                    child: Icon(Icons.location_pin, size: 50.r),
-                  ),
-                ],
-              ),
-              50.verticalSpacingRadius,
-              Obx(
-                () => ConditionalSwitch.single<String>(
-                  context: context,
-                  valueBuilder: (context) => DashboardController.to.statusLocation.value,
-                  caseBuilders: {
-                    'error': (context) => Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Text(
-                              DashboardController.to.messageLocation.value,
-                              style: Get.textTheme.titleLarge,
-                              textAlign: TextAlign.center,
-                            ),
-                            24.verticalSpacingRadius,
-                            ElevatedButton(
-                              onPressed: () => AppSettings.openAppSettings(),
-                              style: ElevatedButton.styleFrom(
-                                primary: Colors.white,
-                                elevation: 2,
-                                padding: EdgeInsets.zero,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(24.r),
-                                ),
-                              ),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  const Icon(
-                                    Icons.settings,
-                                    color: AppColor.darkColor,
-                                  ),
-                                  16.horizontalSpaceRadius,
-                                  Text(
-                                    'Open settings'.tr,
-                                    style: Get.textTheme.bodySmall,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                    'success': (context) => Text(
-                          DashboardController.to.address.value!,
-                          style: Get.textTheme.titleLarge,
-                          textAlign: TextAlign.center,
-                        ),
-                  },
-                  fallbackBuilder: (context) => const CircularProgressIndicator(
-                    color: AppColor.blueColor,
-                  ),
-                ),
+              Image.asset(AssetConst.iconLocation, width: 190.r),
+              Padding(
+                padding: EdgeInsets.all(70.r),
+                child: Icon(Icons.location_pin, size: 50.r),
               ),
             ],
           ),
-        ),
+          50.verticalSpacingRadius,
+          Obx(
+            () => ConditionalSwitch.single<String>(
+              context: context,
+              valueBuilder: (context) => DashboardController.to.statusLocation.value,
+              caseBuilders: {
+                'error': (context) => Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          DashboardController.to.messageLocation.value,
+                          style: Get.textTheme.titleLarge,
+                          textAlign: TextAlign.center,
+                        ),
+                        24.verticalSpacingRadius,
+                        DashboardController.to.messageLocation.value == "Location service not enabled" ||
+                                DashboardController.to.messageLocation.value == "Layanan lokasi dimatikan."
+                            ? ElevatedButton(
+                                onPressed: () => AppSettings.openAppSettings(),
+                                style: ElevatedButton.styleFrom(
+                                  primary: Colors.white,
+                                  elevation: 2,
+                                  padding: EdgeInsets.zero,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(24.r),
+                                  ),
+                                ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Icon(
+                                      Icons.settings,
+                                      color: AppColor.darkColor,
+                                    ),
+                                    16.horizontalSpaceRadius,
+                                    Text(
+                                      'Open settings'.tr,
+                                      style: Get.textTheme.bodySmall,
+                                    ),
+                                  ],
+                                ),
+                              )
+                            : Container(),
+                      ],
+                    ),
+                'success': (context) => Text(
+                      DashboardController.to.address.value!,
+                      style: Get.textTheme.titleLarge,
+                      textAlign: TextAlign.center,
+                    ),
+              },
+              fallbackBuilder: (context) => const CircularProgressIndicator(
+                color: AppColor.blueColor,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
